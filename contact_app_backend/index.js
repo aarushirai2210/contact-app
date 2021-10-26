9b840a73a6d02ca29b3c75c6736787f9579101ae
aarushirai2210 <aarushirai_2000@outlook.com>
2021-10-26 23:38:35 +0530

    routes declared in index file

--- contact_app_backend/index.js
+++ contact_app_backend/index.js
@@ -7,7 +7,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
+const {getContact,createContact} = require('./server/routes')
 
+//get all contact list
+app.get('/get/contact/list',getContact)
+
+//create new contact 
+app.post('/create/new/user',createContact)
 
 app.listen(process.env.PORT,()=>{
     console.log('hello')
